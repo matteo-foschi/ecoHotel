028725814247e5eda313d8bcec6207b0445015ea
Matteo <matteo.foschi@ymail.com>
2022-12-14 08:00:00 +0100

    Aggiornamento

--- changeIP.py
+++ changeIP.py
@@ -3,6 +3,6 @@ import redis
 r = redis.Redis(host='127.0.0.1', port=6379, password='',db=0)
 
 #Simulo che venga cambiato IP rispetto al precedente per l'admin matteofoschi
-r.set("matteofoschi","1.3")
-ip = r.get("matteofoschi")
-print("Ok",ip)
+r.set("administrator","1.3")
+ip = r.get("administrator")
+print("IP Changed",ip)
--- pannello/models.py
+++ pannello/models.py
@@ -8,7 +8,7 @@ class reportData(models.Model):
      produced_energy_in_watt = models.IntegerField(null=True)
      consumed_energy_in_watt = models.IntegerField(null=True)
      report_date = models.DateTimeField(default=timezone.now)
-     hash = models.CharField(max_length=32, default=None, null=True, blank=True)
+     hash = models.CharField(max_length=64, default=None, null=True, blank=True)
      txId = models.CharField(max_length=66, default=None, null=True, blank=True)
 
      def publish(self):
@@ -18,8 +18,8 @@ class reportData(models.Model):
           return self.report_date.strftime("%Y-%m-%d")
 
      def writeOnChain(self):
-          self.hash = hashlib.sha256(str(self.produced_energy_in_watt).encode("utf-8")).hexdigest() + hashlib.sha256(
-               str(self.consumed_energy_in_watt).encode("utf-8")).hexdigest()
+          concatena = str(self.produced_energy_in_watt)+"-"+str(self.consumed_energy_in_watt)
+          self.hash = hashlib.sha256(concatena.encode("utf-8")).hexdigest()
           self.txId = sendTransaction(self.hash)
           self.save()
 
